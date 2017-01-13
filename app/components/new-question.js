5440514ac919e72b4af80988aaa38fe74900ddf3
Alex Francois <apfrancois86@gmail.com>
2017-01-13 11:41:38 -0800

    add save question action

--- app/components/new-question.js
+++ app/components/new-question.js
@@ -1,4 +1,20 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  addNewQuestion: false,
+  actions: {
+    questionFormShow() {
+      this.set('addNewQuestion', true);
+    },
+
+    saveQuestion1() {
+      var params = {
+        question: this.get('question'),
+        author: this.get('author'),
+        comments: this.get('comments'),
+      };
+      this.set('addNewQuestion', false);
+      this.sendAction('saveQuestion2', params);
+    }
+  }
 });
